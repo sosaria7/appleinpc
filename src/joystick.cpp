ca36492d58001b6de503f1f39b8220d69ed72339
Keonwoo Kim <sosaria7@gmail.com>
2016-04-01 22:09:47 +0900

    fixed joystick direction of arrow key

--- src/joystick.cpp
+++ src/joystick.cpp
@@ -81,12 +81,12 @@ BYTE CJoystick::GetStatus(BYTE num)
 			if ( KEYDOWN( DIK_NUMPAD7 )
 				|| KEYDOWN( DIK_NUMPAD8 )
 				|| KEYDOWN( DIK_NUMPAD9 ) 
-				|| KEYDOWN( DIK_DOWN ) )
+				|| KEYDOWN( DIK_UP ) )
 				retval -= 0x7f;
 			if ( KEYDOWN( DIK_NUMPAD1 )
 				|| KEYDOWN( DIK_NUMPAD2 )
 				|| KEYDOWN( DIK_NUMPAD3 ) 
-				|| KEYDOWN( DIK_UP) )
+				|| KEYDOWN( DIK_DOWN ) )
 				retval += 0x80;
 			break;
 			// paddel 2
